da90c237920f7e7ed550cea958527ccfb4556dd0
CyperX <cyperx@CyperXs-MBP.modem>
2019-09-10 14:07:04 +1000

    init

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SongList = () => {
+  return (
+    <div>
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SongList
